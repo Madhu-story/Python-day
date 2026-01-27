53a528c8f85b1ee115552f8f18c28696fd04cb89
Madhu-story <madhura25.rm@gmail.com>
2026-01-27 12:58:05 +0530

    Add Data Types

---
+++ DataTypesDay.py
@@ -0,0 +1,16 @@
+name1 = "rm"
+name2 = 'rm'
+name3 = '''rm'''
+
+print(name1)
+print(name2)
+print(name3)
+
+age = 25
+old = False
+a = None
+
+print(type(name1))
+print(type(age))
+print(type(old))
+print(type(a))
