870aa320875fefc823113ae0b53f29716bc94c25
Madhu-story <madhura25.rm@gmail.com>
2026-01-28 10:09:26 +0530

    Add type conversions

---
+++ typeConversions.py
@@ -0,0 +1,13 @@
+# THere are 2 type conversions:
+
+# 1. Type Conversion:  (Automatically converts the type by python interpreter)
+
+a = 2
+b = 2.25
+print("Answer by Type Conversion method :", a + b)
+
+# 2. Type Casting: (Needs to convert the type manually)
+
+a = int("2")
+b = 2.25
+print("Answer by Type Casting method :", a + b)
